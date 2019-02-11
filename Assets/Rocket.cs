9e51ca3bd16a9e7604b63656da43c86924ef818c
Josh <josh@vrunlimited.com.au>
2019-02-12 09:25:48 +1100

    Lecture 42
    Rocket.cs

---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessUpdate();
+    }
+
+    private void ProcessUpdate()
+    {
+        //Thrust on space key
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+
+        }
+        //Left on 'a' key
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        //Left on 'd' key
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+}
